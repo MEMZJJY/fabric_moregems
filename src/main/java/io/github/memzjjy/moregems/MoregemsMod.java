41492df05fd766a8b83e7c2c3dfb3d0eb2c28fc3
MEMZJJY <931583432@qq.com>
2020-10-04 22:30:52 +0800

    forge和fabric区别真大

--- src/main/java/io/github/memzjjy/moregems/MoregemsMod.java
+++ src/main/java/io/github/memzjjy/moregems/MoregemsMod.java
@@ -3,12 +3,14 @@ package io.github.memzjjy.moregems;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class MoregemsMod implements ModInitializer {
-    public static final Item BLUE_GEM = new Item(new Item.Settings().group(ItemGroup.MISC));
-
+    public static final Item BLUE_GEM = new Item(new Item.Settings().group(ItemGroup.MISC)); //创建蓝宝石物品石粒(doge)
     @Override
     public void onInitialize() {
+        Registry.register(Registry.ITEM, new Identifier("moregems", "blue_gem"), BLUE_GEM);//注册物品
         // This code runs as soon as Minecraft is in a mod-load-ready state.
         // However, some things (like resources) may still be uninitialized.
         // Proceed with mild caution.
--- src/main/java/io/github/memzjjy/moregems/item/BlueGem.java
+++ src/main/java/io/github/memzjjy/moregems/item/BlueGem.java
@@ -8,11 +8,10 @@ import net.minecraft.util.registry.Registry;
 
 
 public class BlueGem implements ModInitializer {
-    public static final Item BLUE_GEM = new Item(new Item.Settings().group(ItemGroup.MISC));
 
     @Override
     public void onInitialize() {
-        Registry.register(Registry.ITEM, new Identifier("moregems", "blue_gem"), BLUE_GEM);
+
 
     }
 }
