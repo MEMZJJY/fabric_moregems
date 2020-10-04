c9faf80a2ccf9d05f25f42a1267f3a569b9be6b2
MEMZJJY <931583432@qq.com>
2020-10-04 22:22:20 +0800

    register blue gem

--- src/main/java/io/github/memzjjy/moregems/MoregemsMod.java
+++ src/main/java/io/github/memzjjy/moregems/MoregemsMod.java
@@ -1,8 +1,12 @@
 package io.github.memzjjy.moregems;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 
 public class MoregemsMod implements ModInitializer {
+    public static final Item BLUE_GEM = new Item(new Item.Settings().group(ItemGroup.MISC));
+
     @Override
     public void onInitialize() {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
