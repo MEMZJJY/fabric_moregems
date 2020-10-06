6668240e2579dc61fdb704f459a8466d7c421e92
MEMZJJY <931583432@qq.com>
2020-10-06 21:15:29 +0800

    add moregems group

--- src/main/java/io/github/memzjjy/moregems/MoregemsMod.java
+++ src/main/java/io/github/memzjjy/moregems/MoregemsMod.java
@@ -1,13 +1,23 @@
 package io.github.memzjjy.moregems;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class MoregemsMod implements ModInitializer {
-    public static final Item BLUE_GEM = new Item(new Item.Settings().group(ItemGroup.MISC)); //创建没屁用的蓝宝石物品石粒(doge)
+    public static final Item BLUE_GEM = new Item(new Item.Settings()); //创建没屁用的蓝宝石物品石粒(doge)
+    public static final ItemGroup MOREGEMS_GROUP = FabricItemGroupBuilder.create(
+            new Identifier("moregems", "moregems_group"))
+            .icon(() -> new ItemStack(BLUE_GEM))
+            .appendItems(stacks -> {
+                stacks.add(new ItemStack(BLUE_GEM));
+            })
+            .build();
     @Override
     public void onInitialize() {
         Registry.register(Registry.ITEM, new Identifier("moregems", "blue_gem"), BLUE_GEM);//注册物品
@@ -17,15 +27,16 @@ public class MoregemsMod implements ModInitializer {
 
         System.out.println("Load moregems main class");
         System.out.println("这是一个恶臭的mod");
-        System.out.println("       　  　\n▃▆█▇▄▖\n" +
-                           "　 　 　 ▟◤▖　　　◥█▎\n" +
-                           "   　 ◢◤　 ▐　　　 　▐▉\n" +
-                           "　 ▗◤　　　▂　▗▖　　▕█▎\n" +
-                           "　◤　▗▅▖◥▄　▀◣　　█▊\n" +
-                           "▐　▕▎◥▖◣◤　　　　◢██\n" +
-                           "█◣　◥▅█▀　　　　▐██◤\n" +
-                           "▐█▙▂　　     　◢██◤\n" +
-                           "◥██◣　　　　◢▄◤\n" +
-                           " 　　▀██▅▇▀");
+        System.out.println(           "\n" +
+                "           ▃▆█▇▄▖\n" +
+                "　 　 　 ▟◤▖　　　◥█▎\n" +
+                "   　 ◢◤　 ▐　　　 　▐▉\n" +
+                "　 ▗◤　　　▂　▗▖　　▕█▎\n" +
+                "　◤　▗▅▖◥▄　▀◣　　█▊\n" +
+                "▐　▕▎◥▖◣◤　　　　◢██\n" +
+                "█◣　◥▅█▀　　　　▐██◤\n" +
+                "▐█▙▂　　     　◢██◤\n" +
+                "◥██◣　　　　◢▄◤\n" +
+                " 　　▀██▅▇▀");
     }
 }
